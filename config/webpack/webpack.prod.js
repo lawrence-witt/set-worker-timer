2c41aeefd77a4ff3c6192152697053f71a231a4f
lazarusWist <lawrence.witt@hotmail.co.uk>
2021-04-01 16:13:06 +0100

    remove module bundle and configs

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -1,49 +1,19 @@
 const path = require('path');
 
-const env = process.env.NODE_ENV.trim();
-
-if (env !== "umd" && env !== "es") {
-    throw new Error('NODE_ENV argument of either umd or es is required.');
-}
-
-const resolveTsConfig = (format) => {
-    return path.resolve(process.cwd(), `config/typescript/tsconfig.${format}.json`);
-};
-
-const settings = {
-    umd: {
-        output: {
-            filename: "index.js",
-            library: {
-                name: "set-worker-timer",
-                type: "umd"
-            }
-        },
-        tsConfig: resolveTsConfig("umd"),
-        experiments: {}
-    },
-    es: {
-        output: {
-            filename: "index.module.js",
-            module: true,
-            library: {
-                type: "module"
-            }
-        },
-        tsConfig: resolveTsConfig("module"),
-        experiments: {
-            outputModule: true
-        }
-    }
-}[env];
+const outputPath = path.resolve(__dirname, "../../dist");
+const tsconfigPath = path.resolve(__dirname, "../typescript/tsconfig.umd.json");
 
 const config = {
     mode: "production",
     entry: "./src/index.ts",
     target: "es5",
     output: {
-        path: path.resolve(process.cwd(), "dist"),
-        ...settings.output
+        path: outputPath,
+        filename: "index.js",
+        library: {
+            name: "set-worker-timer",
+            type: "umd"
+        }
     },
     resolve: {
         extensions: [".ts", ".js"]
@@ -53,18 +23,19 @@ const config = {
             {
                 test: /\.worker\.ts?$/,
                 loader: 'worker-loader',
-                options: { inline: 'no-fallback' }
+                options: { 
+                    inline: 'no-fallback' 
+                }
             },
             {
                 test: /\.ts?$/,
                 loader: "ts-loader",
                 options: {
-                    configFile: settings.tsConfig
+                    configFile: tsconfigPath
                 }
             }
         ]
-    },
-    experiments: settings.experiments
+    }
 }
 
 module.exports = config;
